265037202b8ab56b5f142f03cadb1b3349416260
AntonCh2025 <darth-filth@yandex.ru>
2025-10-03 12:40:56 +0300

    Fix_3: Запуск и завершение работы драйвера перенесены в фикстуру

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -6,29 +6,20 @@ from data.data import Questions
 
 
 class TestMainPage:
-    driver = None
-
-    @classmethod
-    def setup_class(cls):
-        cls.driver = webdriver.Firefox()
 
     @allure.title("Проверка ответов на вопросы")
     @pytest.mark.parametrize('question', Questions.questions)
-    def test_question_answer_correct(self, question):
-        main_page = MainPage(self.driver)
+    def test_question_answer_correct(self, question, browser):
+        main_page = MainPage(browser)
         main_page.open()
         main_page.question_click(question)
         assert main_page.answer_is_visible_and_correct(question)
     
     @allure.title("Проверка открытия Дзена при клике на логотип Яндекса")
-    def test_yandex_logo_click_dzen_opened(self):
-        main_page = MainPage(self.driver)
+    def test_yandex_logo_click_dzen_opened(self, browser):
+        main_page = MainPage(browser)
         main_page.open()
         main_page.yandex_logo_click()
         main_page.switch_to_new_tab()
         assert main_page.is_dzen_page()
-
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
         
