6c0719ca08069b1c18f044b84e1e029788ab7901
AntonCh2025 <darth-filth@yandex.ru>
2025-10-03 13:14:25 +0300

    Fix_4: Локаторы для вопросов/ответов перенесены из data в locators

--- data/data.py
+++ data/data.py
@@ -8,55 +8,54 @@ class Questions:
         {
             "question": "Сколько это стоит? И как оплатить?",
             "answer": "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"Сколько это стоит")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"400 рублей")]']
-
+            "question_locator": MP.question1_locator,
+            "answer_locator": MP.answer1_locator
         },
         {
             "question": "Хочу сразу несколько самокатов! Так можно?",
             "answer": "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто "
                     "сделать несколько заказов — один за другим.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"несколько самокатов")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"заказ — один")]']
+            "question_locator": MP.question2_locator,
+            "answer_locator": MP.answer2_locator
         },
         {
             "question": "Как рассчитывается время аренды?",
             "answer": "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени "
                     "аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в "
                     "20:30, суточная аренда закончится 9 мая в 20:30.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"время аренды?")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"Отсчёт времени")]']
+            "question_locator": MP.question3_locator,
+            "answer_locator": MP.answer3_locator
         },
         {
             "question": "Можно ли заказать самокат прямо на сегодня?",
             "answer": "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"прямо на сегодня?")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"станем расторопнее.")]']
+            "question_locator": MP.question4_locator,
+            "answer_locator": MP.answer4_locator
         },
         {
             "question": "Можно ли продлить заказ или вернуть самокат раньше?",
             "answer": "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"самокат раньше?")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"1010.")]']
+            "question_locator": MP.question5_locator,
+            "answer_locator": MP.answer5_locator
         },
         {
             "question": "Вы привозите зарядку вместе с самокатом?",
             "answer": "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете "
                     "кататься без передышек и во сне. Зарядка не понадобится.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"привозите зарядку")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"полной зарядкой")]']
+            "question_locator": MP.question6_locator,
+            "answer_locator": MP.answer6_locator
         },
         {
             "question": "Можно ли отменить заказ?",
             "answer": "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"отменить заказ?")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"Штрафа не будет")]']
+            "question_locator": MP.question7_locator,
+            "answer_locator": MP.answer7_locator
         },
         {
             "question": "Я жизу за МКАДом, привезёте?",
             "answer": "Да, обязательно. Всем самокатов! И Москве, и Московской области.",
-            "question_locator": [By.XPATH, '//div[contains(text(),"за МКАДом")]'],
-            "answer_locator": [By.XPATH, '//p[contains(text(),"Всем самокатов!")]']
+            "question_locator": MP.question8_locator,
+            "answer_locator": MP.answer8_locator
         }
     ]
 
--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -8,4 +8,26 @@ accept_cookie = [By.XPATH, '//button[@class="App_CookieButton__3cvqF"]']
 yandex_logo = [By.XPATH, '//a[contains(@class,"LogoYandex")]']
 scooter_logo = [By.XPATH, '//a[contains(@class,"LogoScooter")]']
 
-questions_list = [By.XPATH, '']
+question1_locator = [By.XPATH, '//div[contains(text(),"Сколько это стоит")]']
+answer1_locator = [By.XPATH, '//p[contains(text(),"400 рублей")]']
+
+question2_locator = [By.XPATH, '//div[contains(text(),"несколько самокатов")]']
+answer2_locator = [By.XPATH, '//p[contains(text(),"заказ — один")]']
+
+question3_locator = [By.XPATH, '//div[contains(text(),"время аренды?")]']
+answer3_locator = [By.XPATH, '//p[contains(text(),"Отсчёт времени")]']
+
+question4_locator = [By.XPATH, '//div[contains(text(),"прямо на сегодня?")]']
+answer4_locator = [By.XPATH, '//p[contains(text(),"станем расторопнее.")]']
+
+question5_locator = [By.XPATH, '//div[contains(text(),"самокат раньше?")]']
+answer5_locator = [By.XPATH, '//p[contains(text(),"1010.")]']
+
+question6_locator = [By.XPATH, '//div[contains(text(),"привозите зарядку")]']
+answer6_locator = [By.XPATH, '//p[contains(text(),"полной зарядкой")]']
+
+question7_locator = [By.XPATH, '//div[contains(text(),"отменить заказ?")]']
+answer7_locator = [By.XPATH, '//p[contains(text(),"Штрафа не будет")]']
+
+question8_locator = [By.XPATH, '//div[contains(text(),"за МКАДом")]']
+answer8_locator = [By.XPATH, '//p[contains(text(),"Всем самокатов!")]']
